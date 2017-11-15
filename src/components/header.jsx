4dcf33f99daa921e2575baa2642fa03c6195108e
Ken Elliott <makeshift3ds@gmail.com>
2017-11-15 13:52:35 -0500

    sanity header test

---
+++ src/components/header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = () => <main id="header">Header</main>;
+
+export default Header;
---
+++ src/components/header.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './header';
+import { shallow } from 'enzyme';
+
+describe('<Header />', () => {
+  it('should render 1 <Header /> component', () => {
+    const component = shallow(<Header />);
+    expect(component).toHaveLength(1);
+  });
+});
