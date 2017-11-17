f182b33f78234cb7ee0a71522079753e1dd6c310
Ken Elliott <makeshift3ds@gmail.com>
2017-11-17 14:28:35 -0500

    refactor component folder structure

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,6 @@ import './assets/css/main.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.jsx';
+import App from './components/App/app.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
