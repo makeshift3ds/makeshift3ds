8b457e2a2f4ccd4e93e483a5bffd5c066f1660cd
Ken Elliott <makeshift3ds@gmail.com>
2017-11-28 13:24:29 -0500

    added babel-polyfill and transform-class-properties

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import css from './home.css';
 import SectionBlock from '../SectionBlock/section-block.jsx';
 
-const Home = () => (
-  <section className="home">
-    <SectionBlock />
-  </section>
-);
+/* polyfill for async await */
+import BabelPolyfill from 'babel-polyfill';
 
-export default Home;
+export default class Home extends React.Component {
+  state = {
+    posts: []
+  };
+
+  setStateAsync(state) {
+    return new Promise(resolve => {
+      this.setState(state, resolve);
+    });
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://jsonplaceholder.typicode.com/posts');
+      const posts = await res.json();
+      this.setState({
+        posts
+      });
+      console.log(posts);
+    } catch (e) {
+      console.log('error');
+    }
+  }
+
+  render() {
+    return (
+      <section className="home">
+        <SectionBlock />
+      </section>
+    );
+  }
+}
