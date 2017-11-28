da4b3e1c42cda2c1050c4a8a60e3437becee11b0
Ken Elliott <makeshift3ds@gmail.com>
2017-11-28 09:42:54 -0500

    add comment to requestAnimationFrame shim

--- config/jest/shims.js
+++ config/jest/shims.js
@@ -1,3 +1,7 @@
+/** 
+ * add requestAnimationFrame because jest does not provide it and react depends on it. Even in tests.
+ * See Also: https://github.com/facebook/jest/issues/4545 && https://github.com/facebook/jest/issues/1644
+ **/
 global.requestAnimationFrame = callback => {
   setTimeout(callback, 0);
 };
